83e650f7e58788e99536d4d7daaff57ac25296d5
Maillon Andrade <maillon49@gmail.com>
2026-07-02 12:07:44 -0300

    Melhora visual do sistema veterinario

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
 const Nome_do_Usuario = document.getElementById("nomeUsuario");
 const senha_do_Usuario = document.getElementById("senhaUsuario");
+const senha_do_Admin = document.getElementById("senhaAdmin");
+
+const senha_para_cadastro = "molinon"; // Defina a senha do administrador aqui
 
 const botaoLogin = document.getElementById("botaoLogin");
+const botaoCadastro = document.getElementById("botaoCadastro");
 
-let usuarios = [
-    {
-        perfil: "maillon",
-        senha: "123"
-    },
-    {
-        perfil: "julia",
-        senha: "456"
-    }
-];
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
 botaoLogin.addEventListener("click", function() {
+
     for (let i = 0; i < usuarios.length; i++) {
-        if (Nome_do_Usuario.value === usuarios[i].perfil && senha_do_Usuario.value === usuarios[i].senha) {
+        if (Nome_do_Usuario.value === usuarios[i].perfil &&
+            senha_do_Usuario.value === usuarios[i].senha) {
             alert("Login bem-sucedido! Bem-vindo, " + usuarios[i].perfil + "!");
             window.location.href = "menu.html";
             return;
-        } else alert("Nome de usuário ou senha incorretos. Tente novamente.");
+        }
+    }
+    alert("Nome de usuário ou senha incorretos. Tente novamente.");
+});
+
+
+botaoCadastro.addEventListener("click", function() {
+    if (senha_do_Admin.value !== senha_para_cadastro) {
+        alert("Senha do administrador incorreta.");
         return;
     }
+    {
+    for (let i = 0; i < usuarios.length; i++) {
+        if (Nome_do_Usuario.value === usuarios[i].perfil &&
+            senha_do_Usuario.value === usuarios[i].senha) {
+            alert("Usuário já cadastrado. Por favor, faça login.");
+            return;
+        }
+    }
+}
 
-});
+    const novoUsuario = {
+        perfil: Nome_do_Usuario.value,
+        senha: senha_do_Usuario.value
+    }
+    usuarios.push(novoUsuario);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+    alert("Cadastro realizado com sucesso! Agora você pode fazer login.");
+})
